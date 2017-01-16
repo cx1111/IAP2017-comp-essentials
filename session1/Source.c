6855aa7bdc7b141e3cc067f740c20f273d1ac3bb
Chen <is.chenx@gmail.com>
2017-01-16 17:47:11 -0500

    dev

---
+++ session1/Source.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main(){
+	printf("This is my compiled c executable\n");
+}
---
+++ session1/Source.java
@@ -0,0 +1,7 @@
+class Source{
+
+	public static void main(String[] args){
+		System.out.println("This is the java byte code being executed by the Java Virtual Machine");
+	}
+
+}
